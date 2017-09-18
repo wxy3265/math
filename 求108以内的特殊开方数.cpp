29c2807a7651ecbd59d5f603906c440d67b74317
wx3265 <546668033@qq.com>
2017-09-18 21:36:28 +0800

    求108以内的特殊开方数 to 求特殊开方数 And rm a.out

--- 求108以内的特殊开方数.cpp
+++ 求108以内的特殊开方数.cpp
@@ -5,27 +5,30 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
 
-    bool book[109];
+    bool book[n + 1];
     int i;
     int j;
     int ans = 0;
 
-    for (i = 0; i <= 108; i++) {
+    for (i = 0; i <= n; i++) {
         book[i] = false;
     }
 
-    for (i = 2; i <= 10; i++) {
+    for (i = 2; i <= (int)sqrt(n); i++) {
         book[i * i] = true;
     }
 
-    for (i = 1; i <= 108; i++) {
+    for (i = 1; i <= n; i++) {
         if (book[i]) {
             continue;
         } else {
-            for (j = 2; j <= 108; j++) {
+            for (j = 2; j <= n; j++) {
                 if (i % j == 0 && book[j]) {
-                    cout << "\b\b\b\b\b\b\b\b" << i << ':' << sqrt(j) << "根" << i / j << endl;
+                    cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b" 
+                         << i << ':' << sqrt(j) << "根" << i / j << endl;
                     ans++;
                     break;
                 }
